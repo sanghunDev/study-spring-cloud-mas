6a4d2ad7ae3a2e4d5f61286a0fbc05e792b6ba82
sanghun <bsh20057@gmail.com>
2023-07-25 23:55:46 +0900

    feat(회원): 회원 도메인 추가 - 회원 생성 로직 추가

---
+++ user-service/src/main/java/com/app/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.app.domain.user.entity;
+
+import com.app.domain.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 생성 전략을 db에 위임
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String encryptedPassword;
+}
---
+++ user-service/src/main/java/com/app/domain/user/service/UserService.java
@@ -0,0 +1,31 @@
+package com.app.domain.user.service;
+
+import com.app.domain.user.entity.User;
+import com.app.domain.user.repository.UserRepository;
+import com.app.global.error.ErrorCode;
+import com.app.global.error.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User registerUser(User user) {
+        validateDuplicateMember(user);
+        return userRepository.save(user);
+    }
+
+    private void validateDuplicateMember(User user) {
+        Optional<User> userOp = userRepository.findByEmail(user.getEmail());
+        if (userOp.isPresent()) {
+            throw new BusinessException(ErrorCode.ALREADY_REGISTERED_MEMBER);
+        }
+    }
+}
