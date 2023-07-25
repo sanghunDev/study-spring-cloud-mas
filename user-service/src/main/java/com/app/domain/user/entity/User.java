323eb0a91554b507cc910a49369890fec4f46aae
sanghun <bsh20057@gmail.com>
2023-07-26 00:47:35 +0900

    perf(회원): 객체 생성 로직 추가 - Builder 로 생성하도록 처리

--- user-service/src/main/java/com/app/domain/user/entity/User.java
+++ user-service/src/main/java/com/app/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package com.app.domain.user.entity;
 
 import com.app.domain.common.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
@@ -27,4 +28,12 @@ public class User extends BaseEntity {
 
     @Column(nullable = false, unique = true)
     private String encryptedPassword;
+
+    @Builder
+    public User(String email, String name, String userId, String encryptedPassword) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this.encryptedPassword = encryptedPassword;
+    }
 }
